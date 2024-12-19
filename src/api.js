ffb3ea3b0ce733bce837a37d92d63810ebf92fb8
Harsh250307 <160099010+Harsh250307@users.noreply.github.com>
2024-12-19 08:18:01 -0800

    Delete src directory

--- src/api.js
+++
@@ -1,8 +0,0 @@
-
-import axios from 'axios';
-
-const instance = axios.create({
-    baseURL: 'https://didhar-singh-1.onrender.com', // Replace with your Render backend URL
-});
-
-export default instance;
